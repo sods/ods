a66577f81106dbd1e9b33f700ae13f5bf779a9eb
Neil Lawrence <neill@spike.local>
2014-10-08 12:55:12 +0100

    Changes for ambassadors lab class.

--- pods/lab.py
+++ pods/lab.py
@@ -176,7 +176,8 @@ if gdata_available:
                 sheet = self._get_sheet(user)
                 for share in sheet.share_list():
                     if user in share:
-                        sheet.share_modify(user, share_type, send_notifications=False)
+                        # TODO: change sharing here?
+                        #sheet.share_modify(user, share_type, send_notifications=False)
                         return
                 sheet.share([user], share_type, send_notifications)
 
