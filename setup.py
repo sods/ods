bdb7f6748d3910f0ab293c878a7e38f8ca73dba7
Neil Lawrence <lawrennd@gmail.com>
2014-11-12 18:35:54 +0000

    Updates to README.md

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pods',
+  packages = ['pods'], # this must be the same as the name above
+  version = 'v0.01',
+  description = 'Python software for Open Data Science',
+  author = 'The Open Data Science Initiative',
+  author_email = 'lawrennd@gmail.com',
+  url = 'https://github.com/sods/ods', # use the URL to the github repo
+  download_url = 'https://github.com/sods/ods/tarball/v0.01', # I'll explain this in a second
+  keywords = ['datascience', 'open'], # arbitrary keywords
+  classifiers = [],
+)
