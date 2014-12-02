5ef82afdc9902524a52260ec8bb8f421ea979e20
Neil Lawrence <neill@spike.local>
2014-12-02 08:41:28 +0000

    Add R rated classification data set

--- pods/datasets.py
+++ pods/datasets.py
@@ -1102,6 +1102,31 @@ def movie_body_count(data_set='movie_body_count'):
     return data_details_return({'Y': Y, 'info' : "Data set of movies and body count for movies scraped from www.MovieBodyCounts.com created by Simon Garnier and Randy Olson for exploring differences between Python and R.",
                                 }, data_set)
 
+def movie_body_count_r_classify(data_set='movie_body_count'):
+    """Data set of movies and body count for movies scraped from www.MovieBodyCounts.com created by Simon Garnier and Randy Olson for exploring differences between Python and R."""
+    data = movie_body_count()['Y']
+    import pandas as pd
+    import numpy as np
+    X = data[['Year', 'Body_Count']]
+    Y = data['MPAA_Rating']=='R' # set label to be positive for R rated films.
+
+    # Create series of movie genres with the relevant index
+    s = data['Genre'].str.split('|').apply(pd.Series, 1).stack() 
+    s.index = s.index.droplevel(-1) # to line up with df's index
+
+    # Extract from the series the unique list of genres.
+    genres = s.unique()
+
+    # For each genre extract the indices where it is present and add a column to X
+    for genre in genres:
+        index = s[s==genre].index.tolist()
+        X[genre] = np.zeros(X.shape[0])
+        X[genre][index] = np.ones(len(index))
+    return data_details_return({'X': X, 'Y': Y, 'info' : "Data set of movies and body count for movies scraped from www.MovieBodyCounts.com created by Simon Garnier and Randy Olson for exploring differences between Python and R. In this variant we aim to classify whether the film is rated R or not depending on the genre, the years and the body count.",
+                                }, data_set)
+
+
+
 def movielens100k(data_set='movielens100k'):
     """Data set of movie ratings collected by the University of Minnesota and 'cleaned up' for use."""
     if not data_available(data_set):
