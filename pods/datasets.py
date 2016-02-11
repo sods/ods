7936ff03fb13f88cf2f423aa94d3fac6b0529ac2
Alan Saul <alan.daniel.saul@gmail.com>
2016-02-11 10:15:23 +0000

    Updated downloading code, just needs real address putting in

--- pods/datasets.py
+++ pods/datasets.py
@@ -1349,4 +1349,93 @@ def cmu_mocap(subject, train_motions, test_motions=[], sample_every=4, data_set=
         info += ' Data is sub-sampled to every ' + str(sample_every) + ' frames.'
     return data_details_return({'Y': Y, 'lbls' : lbls, 'Ytest': Ytest, 'lblstest' : lblstest, 'info': info, 'skel': skel}, data_set)
 
+def politics_twitter(data_set='politics_twitter'):
+    # Bailout before downloading!
+    import tweepy
+    import pandas as pd
+    import time
+    import progressbar as pb
 
+    if not data_available(data_set):
+        download_data(data_set)
+
+    # FIXME: Try catch here
+    CONSUMER_KEY = config.get('twitter', 'CONSUMER_KEY')
+    CONSUMER_SECRET = config.get('twitter', 'CONSUMER_SECRET')
+
+    OAUTH_TOKEN = config.get('twitter', 'OAUTH_TOKEN')
+    OAUTH_TOKEN_SECRET = config.get('twitter', 'OAUTH_TOKEN_SECRET')
+
+    # Authenticate
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+    # Make tweepy api object, and be carefuly not to abuse the API!
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+    requests_per_minute = int(180.0/15.0)
+    save_freq = 5
+
+    data_dict = {}
+    for party in ['ukip', 'labour', 'conservative', 'greens']:
+        # Load in the twitter data we want to join
+
+        parsed_file_path = os.path.join(data_path, data_set, '{}_twitter_parsed.csv'.format(party))
+        file_already_parsed = False
+        if os.path.isfile(parsed_file_path):
+            print("Data already scraped, loading saved scraped data for {} party".format(party))
+            # Read the data
+            data = pd.read_csv(parsed_file_path)
+            # Check it has been fully parsed (no NATs in time)
+            if data['time'].isnull().sum() == 0:
+                file_already_parsed = True
+
+        if not file_already_parsed:
+            print("Scraping tweet data from ids for the {} party data".format(party))
+            raw_file_path = os.path.join(data_path, data_set, '{}_raw_ids.csv'.format(party))
+            # data = pd.read_csv('./data_download/{}_raw_ids.csv'.format(party))
+            data = pd.read_csv(raw_file_path)
+
+            #Iterate in blocks
+            full_block_size = 100
+            num_blocks = data.shape[0]/full_block_size + 1
+            last_block_size = data.shape[0]%full_block_size
+
+            # Progress bar to give some indication of how long we now need to wait!
+            pbar = pb.ProgressBar(widgets=[
+                    ' [', pb.Timer(), '] ',
+                    pb.Bar(),
+                    ' (', pb.ETA(), ') ',])
+
+            for block_num in pbar(range(num_blocks)):
+                # Get a single block of tweets
+                start_ind = block_num*full_block_size
+                if block_num == num_blocks - 1:
+                    # end_ind = start_ind + last_block_size
+                    tweet_block = data.iloc[start_ind:]
+                else:
+                    end_ind = start_ind + full_block_size
+                    tweet_block = data.iloc[start_ind:end_ind]
+
+                # Gather ther actual data, fill out the missing time
+                tweet_block_ids = tweet_block['id_str'].tolist()
+                tweet_block_results = api.statuses_lookup(tweet_block_ids, trim_user=True)
+                for tweet in tweet_block_results:
+                    data.ix[data['id_str'] == int(tweet.id_str), 'time'] = tweet.created_at
+                
+                # Wait so as to stay below the rate limit
+                # Stay on the safe side, presume that collection is instantanious
+                time.sleep(60.0/requests_per_minute + 0.1)
+
+                if block_num % save_freq == 0:
+                    data.to_csv(parsed_file_path)
+                
+            #Now convert times to pandas datetimes
+            data['time'] = pd.to_datetime(data['time'])
+            #Get rid of non-parsed dates
+            data = data.ix[data['time'].notnull(), :]
+            data.to_csv(parsed_file_path)
+
+        data_dict[party] = data
+    
+    return data_details_return(data_dict, data_set)
