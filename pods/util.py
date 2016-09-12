ba6494aa7d59673dcc3496cb9c56f96c5b82b93b
mzwiessele <ibinbei@gmail.com>
2016-09-12 11:53:56 +0100

    fix: fixed printing issue for downloads with py27

--- pods/util.py
+++ pods/util.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-from __future__ import absolute_import
+from __future__ import absolute_import, division
 import sys
 import os
 
@@ -41,35 +41,63 @@ def download_url(url, dir_name='.', save_name=None, store_directory=None, messag
         meta = response.info()
         content_length_str = meta.get("Content-Length")
         if content_length_str:
-            if sys.version_info>=(3,0):
+            #if sys.version_info>=(3,0):
+            try:
                 file_size = int(content_length_str)
-            else:
-                file_size = int(content_length_str[0])
+            except:
+                try: 
+                    file_size = int(content_length_str[0])
+                except:
+                    file_size = None
+            if file_size == 1:
+                file_size = None
+            #else:
+            #    file_size = int(content_length_str)
         else:
             file_size = None
+        
         status = ""
         file_size_dl = 0
         block_sz = 8192
-        line_length=30
+        line_length = 30
+        percentage = 1./line_length
+        
+        if file_size:
+            print("|"+"{:^{ll}}".format("Downloading {:7.3f}MB".format(file_size/(1048576.)), ll=line_length)+"|")
+            from itertools import cycle
+            cycle_str = cycle('>')
+            sys.stdout.write("|")
+                    
         while True:
             buff = response.read(block_sz)
             if not buff:
                 break
             file_size_dl += len(buff)
             f.write(buff)
-            sys.stdout.write(" "*(len(status)) + "\r")
+            
             # If content_length_str was incorrect, we can end up with many too many equals signs, catches this edge case
-            correct_meta = float(file_size_dl)/file_size <= 1.0
-            if file_size and correct_meta:
-                percentage = "="*int(line_length*float(file_size_dl)/file_size)
-                status = r"[{perc: <{ll}}] {dl:7.3f}/{full:.3f}MB".format(dl=file_size_dl/(1048576.), full=file_size/(1048576.), ll=line_length, perc=percentage)
+            #correct_meta = float(file_size_dl)/file_size <= 1.0
+            
+            if file_size:
+                if (float(file_size_dl)/file_size) >= percentage:
+                    sys.stdout.write(next(cycle_str))
+                    sys.stdout.flush()
+                    percentage += 1./line_length
+                #percentage = "="*int(line_length*float(file_size_dl)/file_size)
+                #status = r"[{perc: <{ll}}] {dl:7.3f}/{full:.3f}MB".format(dl=file_size_dl/(1048576.), full=file_size/(1048576.), ll=line_length, perc=percentage)
             else:
-                status = r"[{perc: <{ll}}] {dl:7.3f}MB".format(dl=file_size_dl/(1048576.),
+                sys.stdout.write(" "*(len(status)) + "\r")
+                status = r"{dl:7.3f}MB".format(dl=file_size_dl/(1048576.),
                                                                        ll=line_length,
                                                                        perc="."*int(line_length*float(file_size_dl/(10*1048576.))))
+                sys.stdout.write(status)
+                sys.stdout.flush()
 
-            sys.stdout.write(status)
+            #sys.stdout.write(status)
+        
+        if file_size:
+            sys.stdout.write("|")
             sys.stdout.flush()
-        sys.stdout.write(" "*(len(status)) + "\r")
+
         print(status)
         # if we wanted to get more sophisticated maybe we should check the response code here again even for successes.
