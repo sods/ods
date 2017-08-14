5c967c2aa945426698e5f209fdbf5b11e48e9e92
Neil Lawrence <lawrennd@gmail.com>
2017-08-14 01:08:55 +0100

    Add travis_tests

---
+++ travis_tests.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+import nose, warnings
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    nose.main('pods', defaultTest='pods/testing', argv=['', '--show-skipped'])
