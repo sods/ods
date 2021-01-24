1fba89d5d050a7d28c47111ea9cbde6635339b18
Neil Lawrence <lawrennd@gmail.com>
2021-01-24 18:46:33 +0000

    Update image display functionality for compatability with google colab.

--- pods/notebook.py
+++ pods/notebook.py
@@ -53,7 +53,7 @@ def display_iframe_url(target, **kwargs):
     display(HTML(txt))
 
 
-def display_google_book(id, page=None, width=700, height=500, **kwargs):
+def display_google_book(id, page=None, width=600, height=450, **kwargs):
     """Display an embedded version of a Google book.
     :param id: the id of the google book to display.
     :type id: string
@@ -196,9 +196,9 @@ def display_prediction(basis, num_basis=4, wlim=(-1.,1.), fig=None, ax=None, xli
              **param_args)
 
 
-def display_plots(filebase, directory=None, width=700, height=500, **kwargs):
+def display_plots(filebase, directory=None, width=600, height=40, **kwargs):
     """Display a series of plots controlled by sliders. The function relies on Python string format functionality to index through a series of plots."""
-    def show_figure(filebase, directory, **kwargs):
+    def show_figure(filebase, directory, width=600, height=450,  **kwargs):
         """Helper function to load in the relevant plot for display."""
         _, ext = os.path.splitext(filebase)
         filename = filebase.format(**kwargs)
@@ -211,5 +211,5 @@ def display_plots(filebase, directory=None, width=700, height=500, **kwargs):
         elif ext.lower() in ['.html']:
             display(IFrame(src="{filename}".format(filename=filename), width=width, height=height))
             
-    interact(show_figure, filebase=fixed(filebase), directory=fixed(directory), **kwargs)
+    interact(show_figure, filebase=fixed(filebase), directory=fixed(directory), width=fixed(width), height=fixed(height), **kwargs)
 
