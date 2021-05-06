6e31a84f5167f3d62ab5c6b0868cb5167f8adb82
Neil Lawrence <lawrennd@gmail.com>
2021-05-06 09:53:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,156 @@
-from setuptools import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# https://github.com/navdeep-G/setup.py
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pipenv install twine --dev
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+
+# Package meta-data.
+NAME = 'pods'
+DESCRIPTION = 'Python software for Open Data Science'
+URL = 'https://github.com/lawrennd/ods'
+EMAIL = 'lawrennd@gmail.com'
+AUTHOR = 'Neil D. Lawrence'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = 'v0.0.21-alpha'
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'openpyxl', 'pods', 'pandas', 'lxml', 'gdata',
+]
+
+# What packages are optional?
+EXTRAS = {
+    'google docs interface': [
+        'gspread',
+        'httplib2',
+        'oauth2client',
+        'google-api-python-client'
+    ],
+    'pandas google sheet interface' : [
+        'gdata'
+    ],
+    
+}
+
+PACKAGE_DATA = {'pods': ['defaults.cfg', 'data_resources.json', 'football_teams.json']}
+
+DEPENDENCY_LINKS = []
+
+CONSOLE_SCRIPTS = []
+ENTRY_POINTS = {
+    'console_scripts': CONSOLE_SCRIPTS,
+}
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+# Where the magic happens:
 setup(
-  name = 'pods',
-  packages = ['pods'], # this must be the same as the name above
-  package_data={'pods': ['defaults.cfg', 'data_resources.json', 'football_teams.json']},
-  version = 'v0.0.21-alpha',
-  description = 'Python software for Open Data Science',
-  author = 'The Open Data Science Initiative',
-  author_email = 'lawrennd@gmail.com',
-  url = 'https://github.com/sods/ods', # use the URL to the github repo
-  download_url = 'https://github.com/sods/ods/tarball/v0.0.21a0', 
-  keywords = ['datascience', 'open', 'google sheet'], 
-  classifiers = [],
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    package_data=PACKAGE_DATA,
+
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    entry_points=ENTRY_POINTS,          
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    dependency_links=DEPENDENCY_LINKS,
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
