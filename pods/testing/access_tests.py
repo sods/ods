7768540d72c51a2dc267fe22e42f924074614957
Neil Lawrence <lawrennd@gmail.com>
2021-08-26 15:04:57 +0100

    Update name of access tests

--- pods/testing/access_tests.py
+++ pods/testing/access_tests.py
@@ -24,7 +24,7 @@ bogus_file = "http://www.bbc.co.uk/bogus_file.html"
 
 
 def test_downloard_url():
-    """util_tests: Test downloading of a URL."""
+    """access_tests: Test downloading of a URL."""
 
     pods.access.download_url(
         test_url, save_name=save_name, store_directory=store_directory
@@ -46,13 +46,13 @@ def test_downloard_url():
 
 @raises(URLError, ValueError)
 def test_graceful_failure_fake_url():
-    """util_tests: Test graceful failure of a fake url."""
+    """access_tests: Test graceful failure of a fake url."""
     pods.access.download_url(fake_url)
 
 
 @raises(HTTPError, ValueError)
 def test_graceful_failure_bogus_file():
-    """util_tests: Test graceful failure of a bogus file."""
+    """access_tests: Test graceful failure of a bogus file."""
     pods.access.download_url(bogus_file)
 
 
@@ -60,9 +60,9 @@ import filecmp
 import unittest
 
 
-class UtilTests(unittest.TestCase):
+class AccessTests(unittest.TestCase):
     def test_download_url(self):
-        """util_tests: Test the download url"""
+        """access_tests: Test the download url"""
         filename = "access.py"
         download_name = "tmp.py"
         # Download the file to current directory
